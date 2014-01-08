1c4c0c0ddcfe5e04711ead7fd127f714ff7dacf7
Riveascore <agaxx010@morris.umn.edu>
2014-01-07 21:13:16 -0600

    Initial commit, did basic setup, created products migration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CombatCabana::Application.config.secret_key_base = '239c82530ece0ef2ac49df367900760b43172bd33e96069fb7b861bfca512da122b1ead41c05450e8022db1d0b61902b2eba4042d3380096acc9a4eaa19f1ce2'
---
+++ db/seeds.rb
@@ -0,0 +1,34 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+
+Product.create!(
+	title: 'Barrel Cover',
+	description: %{
+		<p>Covers your barrel so you don't
+		accidentally shoot someone's eye out.</p>},
+	image_url: "http://hudsonvalleyairsoft.com/images/cover.jpg",
+	price: 5.0
+	)
+
+Product.create!(
+	title: 'Save Phace Goggles',
+	description: %{
+		<p>Non-fogging goggles, high quality.</p>},
+	image_url: "http://ecx.images-amazon.com/images/I/41-iKgjY83L._SY300_.jpg",
+	price: 5.0
+	)
+
+Product.create!(
+	title: 'Ultrasonic BBs',
+	description: %{
+		<p>10,000 BBs, yellow.</p>},
+	image_url: "http://www.big5sportinggoods.com/photos/product/giant/4782420S163580/airsoft-pellets/10-000-airsoft-bbs-with-bonus-airsoft-pistol.jpg",
+	price: 5.0
+	)
